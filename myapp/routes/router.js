2b7b68f8714ea11bb9f645b16dfed8b50d96ff4c
Kellan Ryan <KellanRyan0707@gmail.com>
2019-08-22 16:36:04 -0500

    new backend again

--- myapp/routes/router.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var mongoose = require( 'mongoose' );
-
-router.get('/',function(req,res,next){    
-    res.render('Starter',{title:"DreamScape"});    
-});
-
-module.exports = router;
