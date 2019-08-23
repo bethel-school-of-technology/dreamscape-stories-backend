449415c7ccb4959c93becc211e2c5430f364aca8
Kellan Ryan <KellanRyan0707@gmail.com>
2019-08-22 21:17:43 -0500

    updated mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PostsController } from './posts/posts.controller';
 import { PostsService } from './posts/posts.service';
+import { PostsModule } from './posts/posts.module';
+import config from './config/keys';
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot({
-    rootPath: join(__dirname, '..', 'client'),
+    PostsModule,
+    MongooseModule.forRoot(config.mongoURI),
+    ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'client'),
   }),
 ],
   controllers: [AppController, PostsController],
---
+++ src/config/keys.ts
@@ -0,0 +1,3 @@
+export default {
+    mongoURI: 'mongodb+srv://KellanRyan:B07e07t94h!@cluster0-zjlni.mongodb.net/test?retryWrites=true&w=majority'
+}
