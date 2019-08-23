e3637a64cd0b65a1a7752e74d3752fff9c97f32c
Kellan Ryan <KellanRyan0707@gmail.com>
2019-08-22 21:24:42 -0500

    schema

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
+import { PostSchema } from './schemas/post.schemas'
 
 @Module({
   imports: [],
