7bd5e2c88b624d2fa60c9afef20e7be61ed83aaa
Kellan Ryan <KellanRyan0707@gmail.com>
2019-08-25 11:04:32 -0500

    mongo.uri update

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,12 @@ import { postsModule } from './posts/posts.module';
 import { AppController } from './app.controller';
 import { postsController } from './posts/posts.controller.spec';
 import { AppService } from './app.service';
-import { postsService } from './posts/posts.service';
 import config from './config/keys'
+import { postsService } from './posts/posts.service';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(config.mongoURI),
+    MongooseModule.forRoot(config.mongoURI,{useNewUrlParser: true }),
     postsModule,
   ],
   controllers: [
