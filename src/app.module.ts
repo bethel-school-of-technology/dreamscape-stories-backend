93140c17123bac344e65fae6fb76074feff130fd
Kellan Ryan <KellanRyan0707@gmail.com>
2019-08-26 21:25:21 -0500

    auth update

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,16 @@ import { postsModule } from './posts/posts.module';
 import { AppController } from './app.controller';
 import { postsController } from './posts/posts.controller.spec';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 import config from './config/keys'
-import { postsService } from './posts/posts.service';
 
 @Module({
   imports: [
     MongooseModule.forRoot(config.mongoURI,{useNewUrlParser: true }),
     postsModule,
+    AuthModule,
+    UsersModule,
   ],
   controllers: [
     AppController,
