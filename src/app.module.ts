df88c51c141ced920b208a5d1bbbd6907993604b
Kellan Ryan <KellanRyan0707@gmail.com>
2019-08-28 22:44:07 -0500

    static serve import

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,13 @@ import { postsController } from './posts/posts.controller.spec';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import config from './config/keys'
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({rootPath: join(__dirname, '..', 'client')}),
     MongooseModule.forRoot(config.mongoURI,{useNewUrlParser: true }),
     postsModule,
     AuthModule,
